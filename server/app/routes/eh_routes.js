486493893ffce8d0e6e558fa32186dab9120510d
angrymuskrat <mlhakov2011@gmail.com>
2018-06-08 10:17:44 +0300

    cosmetic improvements

--- server/app/routes/eh_routes.js
+++ server/app/routes/eh_routes.js
@@ -1,7 +1,19 @@
-function sendAnswer(res, status, errorCode) {
-    //console.log(JSON.stringify({ status: status, errorCode: errorCode }));
-    res.send(JSON.stringify({ status: status, errorCode: errorCode }));
-};
+function statusMessage(status, errorCode) {
+    return {
+        status: status,
+        errorCode: errorCode
+    }
+}
+const ErrorMessage = statusMessage.bind(null, "ERROR");
+
+const SUCCESS = statusMessage("SUCCESS", null);
+const DB_INSERT_ERROR = ErrorMessage(32);
+const DB_FINDING_ERROR = ErrorMessage(33);
+const UNKNOWN_TOKEN = ErrorMessage(34);
+
+function sendAnswer(res, obj) {
+    res.send(JSON.stringify(obj));
+}
 
 function genToken() {
     let token = "";
@@ -11,131 +23,114 @@ function genToken() {
     return token;
 }
 
-
+function makeLog(label) {
+    return (msg) => console.log(label + " " + msg);
+}
 
 module.exports = function(App, db) {
+    let dbo = db.db("test");
 
-    this.myDb = db;
+    function checkToken(send, obj, func) {
+        let log = function (msg) { console.log("checkToken " + msg) };
+        dbo.collection("users").findOne({ token : obj.token }, (err, result) => {
+            log("checking user");
+            if (err) {
+                log(err);
+                send(DB_FINDING_ERROR);
+                return;
+            }
+            if (result === null) {
+                log("unknown token");
+                send(UNKNOWN_TOKEN);
+                return;
+            }
+            log("checked user");
+            func();
+        });
+    }
+    
     App.post('/signIn', (req, res) => {
         let send = sendAnswer.bind(null, res);
-        let gtoken =  req.body.googleToken;
-        console.log("/signIn: " + gtoken);
-        let dbo = myDb.db("testdb");
-        dbo.collection("users").findOne({ googleToken: gtoken}, (err, result) => {
+        let user = req.body;
+        let log = makeLog("signIn")
+        log(user.googleToken);
+        dbo.collection("users").findOne({ googleToken: user.googleToken }, (err, result) => {
             if (err) {
-                send("ERROR", 33);
-                console.log(err);
-            }//33 db find error
-            else {
-                console.log("find element");
-                let token, first = false;
-                if (result === null) {
-                    first = true;
-                    dbo.collection("users").insertOne(
-                        {
-                            googleToken: gtoken,
-                            token: token = genToken()
-                        }, (err, res) => {
-                            if (err) send("ERROR", 32);
-                            console.log("insert new element " + token);
-                        });
-                }
-                else token = result.token;
-                res.send(JSON.stringify({
-                    token: token,
-                    isFirst: first
-                }));
+                log(err);
+                send(DB_FINDING_ERROR);
+                return;
             }
-            //myDb.close();
+            let token, first = false;
+            if (result === null) {
+                first = true;
+                log("adding new user");
+                user.token = token = genToken();
+                dbo.collection("users").insertOne(user, (err, res) => {
+                        if (err) {
+                            log(err);
+                            send(DB_INSERT_ERROR);
+                            return;
+                        }
+                        log("added new user");
+                    });
+            }
+            else token = result.token;
+            send({ token: token, isFirst: first });
         });
     });
 
     App.post('/addEvent', (req, res) => {
         let send = sendAnswer.bind(null, res);
-        let dbo = myDb.db("testdb");
         let event = req.body;
+        let log = makeLog("addEvent");
         event.latitude = Math.floor(event.latitude * 1000000);
         event.longitude = Math.floor(event.longitude * 1000000);
-        console.log("/addEvent " + event.name);
-        dbo.collection("users").findOne({ token: event.token }, (err, result) => {
-            console.log("checking user")
-            if (err) send("ERROR", 33);
-            if (result === null) {
-                send("ERROR", 34) // unknown token
-            }
-            else {
-                console.log("checked user")
-                dbo.collection("events").insertOne(event, (err, res) => {
-                    if (err) send("ERROR", 33);
-                    console.log("events added " + event.name);
-                    send("SUCCESS", null);
-                });
-            }
+        log(event.name);
+        checkToken(send, event, () => {
+            dbo.collection("events").insertOne(event, (err) => {
+                if (err) {
+                    log(err);
+                    send(DB_INSERT_ERROR);
+                    return;
+                }
+                send(SUCCESS);
+                log("event " + event.name + " added");
+            })
         });
     });
 
     App.post('/getEvents', (req, res) => {
         let send = sendAnswer.bind(null, res);
-        let dbo = myDb.db("testdb");
-        let event = req.body;
-        event.latitude = Math.floor(event.latitude * 1000000);
-        event.longitude = Math.floor(event.longitude * 1000000);
+        let require = req.body;
+        let log = makeLog("getEvent");
+        log('');
+        require.latitude = Math.floor(require.latitude * 1000000);
+        require.longitude = Math.floor(require.longitude * 1000000);
         const latitudeUpset = 100000;
         const longitudeUpset = 170000;
-        dbo.collection("users").findOne({ token: event.token }, (err, result) => {
-            console.log("checking user")
-            if (err) send("ERROR", 33);
-            if (result === null) {
-                send("ERROR", 34) // unknown token
-            }
-            else {
-                console.log("checked user");
-                const q1 = event.latitude - latitudeUpset;
-                const q2 = event.latitude * 1  + latitudeUpset;
-                console.log(q1 + " " + q2);
-                dbo.collection("events").find({
-                    category: event.category,
-                    subcategory: event.subcategory,
-                    eventDate: { $gte: event.userDate, $lte: event.eventDate },
-                    cost: { $lte: event.cost },
-                    latitude: {
-                        $gt: event.latitude - latitudeUpset,
-                        $lt: event.latitude + latitudeUpset
-                    },
-                    longitude: {
-                        $gt: event.longitude - longitudeUpset,
-                        $lt: event.longitude + longitudeUpset
-                    }
-                }).toArray((err, result) => {
-                    if (err) send("ERROR", 33);
-                    res.send(JSON.stringify({ data : result }));
-                });
-            }
-        });
-    });
-
-    App.post('/signUp', (req, res) => {
-        let send = sendAnswer.bind(null, res);
-        let user = {
-            googleToken: req.body.googleToken,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            photo: req.body.photo,
-            token: genToken()
-        };
-        console.log("/signUp:" + user.firstName + " " + user.lastName + " "
-            + user.googleToken);
-        let dbo = myDb.db("testdb");
-        console.log(dbo);
-        dbo.collection('users').insertOne(user, (err, result) => {
-            if (err) {
-                console.log("db error: " + err);
-                send("ERROR", 32) //32 - db insert error
-            } else {
-                console.log("success insert in db");
-                send("SUCCESS", null);
-            }
-            myDb.close();
+        checkToken(send, require, () => {
+            let filter = {
+                eventDate: { $gte: require.userDate, $lte: require.eventDate },
+                cost: { $lte: require.cost },
+                latitude: {
+                    $gt: require.latitude - latitudeUpset,
+                    $lt: require.latitude + latitudeUpset
+                },
+                longitude: {
+                    $gt: require.longitude - longitudeUpset,
+                    $lt: require.longitude + longitudeUpset
+                }
+            };
+            if (require.category !== 'Any') filter.category = require.category;
+            if (require.subcategory !== 'Any') filter.subcategory = require.subcategory;
+            dbo.collection("events").find(filter).toArray((err, result) => {
+                if (err) {
+                    log(err);
+                    send(DB_FINDING_ERROR);
+                }
+                send({ data : result });
+                log("success require");
+            });
         });
     });
 };
--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const db           = require('./config/db');
 const port = 8000;
 
 app.use(bodyParser.json({})); // imit - default value 100kb, value in bytes
-require('./app/routes')(app, {});
+//require('./app/routes')(app, {});
 MongoClient.connect(db.url, { useNewUrlParser: true } , (err, db) => {
     if (err) return console.log(err + "shit");
     require('./app/routes')(app, db);
